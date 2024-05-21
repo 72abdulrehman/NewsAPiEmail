12dac6767c5dbe594648efc262e136ddbd0dd246
72abdulrehman <abdulrehmanikram9@gmail.com>
2024-05-21 12:54:25 +0500

    Made five changes 1-Add links, 2-Specified langusge, 3-Limit news, 4-Add subject, 5-Control catogery

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
 api_key = "f2e37f1122ba4e31815adafbc7c780f2"
+url = "https://newsapi.org/v2/everything?"\
+        f"q={topic}&from=2024-04-20&sortBy=publishedAt&"\
+        "apiKey=f2e37f1122ba4e31815adafbc7c780f2"\
+        "&language=en"
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-04-20&sortBy=publishedAt&apiKey=f2e37f1122ba4e31815adafbc7c780f2"
-
+# Making Request
 request = requests.get(url)
+
+# Get a dictionary with data
 content = request.json()
 
-body = ""
-for article in content["articles"]:
-    body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+# Access the article features
+body = "Subject: Today's news" + "\n"
+for article in content["articles"][:20]:
+    body = body + article["title"] + "\n" \
+        + str(article["description"]) \
+        + "\n" + article["url"] + 2*"\n"
     
 body = body.encode("utf-8") 
 send_email(message=body)
