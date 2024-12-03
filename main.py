2125cf28d15410d14f385e2b9ae7432a6a53bc26
AbdulRehman <97100802+72abdulrehman@users.noreply.github.com>
2024-12-03 13:25:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from send_email import send_email
 
 topic = "tesla"
-api_key = "f2e37f1122ba4e31815adafbc7c780f2"
+api_key = ""
 url = "https://newsapi.org/v2/everything?"\
         f"q={topic}&from=2024-04-20&sortBy=publishedAt&"\
         "apiKey=f2e37f1122ba4e31815adafbc7c780f2"\
